d7dc6d0daed55c652b5ea5b91183f27d833d215a
ibwei <bawei3600@gmail.com>
2023-10-29 16:36:34 +0800

    feat(git): update git info

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,25 +1,18 @@
-import { arbitrum, arbitrumGoerli } from '@wagmi/chains'
+import { arbitrum, arbitrumGoerli, mainnet, polygonMumbai } from '@wagmi/chains'
 import { sample } from 'lodash'
 
-export const CHAINS = [arbitrum, arbitrumGoerli]
+export const TEST_CHAINS = [arbitrumGoerli, polygonMumbai]
+export const MAINNET_CHAINS = [arbitrum, mainnet]
+export const CHAINS = [...TEST_CHAINS, ...MAINNET_CHAINS]
 export const CHAINS_IDS = CHAINS.map((chain) => chain.id)
-
-export const ARBITRUM = arbitrum
-export const ARBITRUM_CHAIN_ID = arbitrum.id
-export const ARBITRUM_GOERLI = arbitrumGoerli
-export const ARBITRUM_GOERLI_CHAIN_ID = arbitrumGoerli.id
 
 export enum SuppportedChainId {
   ARBITRUM_CHAIN_ID = arbitrum.id,
   ARBITRUM_GOERLI_CHAIN_ID = arbitrumGoerli.id,
 }
 
-export const DEFAULT_CHAIN_ID = ARBITRUM_GOERLI_CHAIN_ID
+export const DEFAULT_CHAIN_ID = arbitrumGoerli.id
 
-export const IS_NETWORK_DISABLED = {
-  [ARBITRUM_CHAIN_ID]: false,
-  [ARBITRUM_GOERLI_CHAIN_ID]: false,
-}
 export const MAX_GAS_PRICE_MAP: { [key in SuppportedChainId]: string } = {
   [SuppportedChainId.ARBITRUM_CHAIN_ID]: '0',
   [SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID]: '0',
@@ -31,28 +24,28 @@ export const GAS_PRICE_ADJUSTMENT_MAP: { [key in SuppportedChainId]: string } = 
 }
 
 export const CHAIN_PER_BLOCK = {
-  [ARBITRUM_CHAIN_ID]: 1,
-  [ARBITRUM_GOERLI_CHAIN_ID]: 1,
+  [SuppportedChainId.ARBITRUM_CHAIN_ID]: 1,
+  [SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID]: 1,
 }
 
 export const RPC_URLS = {
-  [ARBITRUM_CHAIN_ID]: ['https://arb1.arbitrum.io/rpc', 'https://arb-mainnet.g.alchemy.com/v2/EmVYwUw0N2tXOuG0SZfe5Z04rzBsCbr2'],
-  [ARBITRUM_GOERLI_CHAIN_ID]: ['https://arbitrum-goerli.public.blastapi.io'],
+  [SuppportedChainId.ARBITRUM_CHAIN_ID]: ['https://arb1.arbitrum.io/rpc', 'https://arb-mainnet.g.alchemy.com/v2/EmVYwUw0N2tXOuG0SZfe5Z04rzBsCbr2'],
+  [SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID]: ['https://arbitrum-goerli.public.blastapi.io'],
 }
 
 export const RPC_PROVIDERS = {
-  [ARBITRUM_CHAIN_ID]: [RPC_URLS[ARBITRUM_CHAIN_ID][0]],
-  [ARBITRUM_GOERLI_CHAIN_ID]: [RPC_URLS[ARBITRUM_GOERLI_CHAIN_ID][0]],
+  [SuppportedChainId.ARBITRUM_CHAIN_ID]: [RPC_URLS[SuppportedChainId.ARBITRUM_CHAIN_ID][0]],
+  [SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID]: [RPC_URLS[SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID][0]],
 }
 
 export const FALLBACK_PROVIDERS = {
-  [ARBITRUM_CHAIN_ID]: [RPC_URLS[ARBITRUM_CHAIN_ID][1]],
-  [ARBITRUM_GOERLI_CHAIN_ID]: [RPC_URLS[ARBITRUM_GOERLI_CHAIN_ID][1]],
+  [SuppportedChainId.ARBITRUM_CHAIN_ID]: [RPC_URLS[SuppportedChainId.ARBITRUM_CHAIN_ID][1]],
+  [SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID]: [RPC_URLS[SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID][1]],
 }
 
 export const CHAIN_INFO = {
-  [ARBITRUM_CHAIN_ID]: ARBITRUM,
-  [ARBITRUM_GOERLI_CHAIN_ID]: ARBITRUM_GOERLI,
+  [SuppportedChainId.ARBITRUM_CHAIN_ID]: arbitrum,
+  [SuppportedChainId.ARBITRUM_GOERLI_CHAIN_ID]: arbitrumGoerli,
 }
 
 export function getRpcUrl(chainId: number): string | undefined {
